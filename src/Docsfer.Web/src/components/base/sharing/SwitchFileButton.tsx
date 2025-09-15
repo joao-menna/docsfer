0e82dbf8a47ef7185fe1aa7b9cbdce8bdffa51f6
Joao-AZ <jpmizidoro@gmail.com>
2025-09-15 20:58:14 +0000

    fix: switch had a form

--- src/Docsfer.Web/src/components/base/sharing/SwitchFileButton.tsx
+++ src/Docsfer.Web/src/components/base/sharing/SwitchFileButton.tsx
@@ -2,7 +2,7 @@ import * as Switch from "@radix-ui/react-switch";
 import clsx from "clsx";
 
 const SwitchFileButton = () => (
-  <form>
+  <div>
     <div className="flex items-center">
       <Switch.Root
         className={clsx(
@@ -14,7 +14,7 @@ const SwitchFileButton = () => (
         <Switch.Thumb className="block size-[21px] translate-x-0.5 rounded-full bg-white shadow-[0_2px_2px] shadow-blackA4 transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[19px]" />
       </Switch.Root>
     </div>
-  </form>
+  </div>
 );
 
 export default SwitchFileButton;
