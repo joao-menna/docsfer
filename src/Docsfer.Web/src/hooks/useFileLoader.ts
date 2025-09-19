6958c3d3680b7e79e1696b7ed1c978b4539f3558
Joao-AZ <jpmizidoro@gmail.com>
2025-09-19 21:03:40 +0000

    apaguei tudo sem querer bosta

--- src/Docsfer.Web/src/hooks/useFileLoader.ts
+++ src/Docsfer.Web/src/hooks/useFileLoader.ts
@@ -6,8 +6,32 @@ export const filesLoader = async () => {
   return {
     files: [
       {
-        id: "123",
-        name: "brabo",
+        id: 123,
+        name: "Documento1.pdf",
+        creationDate: "15 Abr 2025 16:00",
+        modifyDate: "16 Abr 2025 17:00",
+        uploeader: "Ricardo",
+        Groups: [
+          {
+            name: "RH",
+            permission: "write",
+          },
+          {
+            name: "Administrativo",
+            permission: "admin",
+          },
+        ],
+        Users: [
+          {
+            name: "Joao",
+          },
+          {
+            name: "Ricardo",
+          },
+          {
+            name: "Henrique",
+          },
+        ],
       },
     ],
   };
--- src/Docsfer.Web/src/pages/Arquivos/FileDetailsPage.tsx
+++ src/Docsfer.Web/src/pages/Arquivos/FileDetailsPage.tsx
@@ -1,8 +1,23 @@
-import { useLoaderData } from "react-router";
+import { X } from "lucide-react";
+import { useLoaderData, useParams } from "react-router";
+
+type Group = {
+  name: string;
+  permission: "read" | "write" | "admin";
+};
+
+type User = {
+  name: string;
+};
 
 type File = {
   id: string;
   name: string;
+  creationDate: string;
+  modifyDate: string;
+  uploader: string;
+  groups?: Group[];
+  users?: User[];
 };
 
 type LoaderData = {
@@ -10,15 +25,52 @@ type LoaderData = {
 };
 
 export default function FileDetails() {
-  const { files } = useLoaderData() as LoaderData;
+  const { files } = useLoaderData<LoaderData>();
+  const { fileId } = useParams();
 
-  const file = files[0];
+  const file = files.find((f) => f.id === fileId) ?? files[0];
+  // pra mockar a tela de erro só tirar o ?? files[0];
+
+  if (!file) {
+    return (
+      <div className="w-dvw h-dvh flex items-center justify-center font-gabarito text-red-500/50">
+        Nenhum arquivo encontrado, desculpe.
+      </div>
+    );
+  }
 
   return (
-    <div className="text-zinc-400 font-gabarito">
-      <h1>File details</h1>
-      <p>ID: {file.id}</p>
-      <p>Name: {file.name}</p>
+    <div className="flex justify-between px-6 py-4">
+      {/* file content */}
+      <div className="flex flex-col gap-10 min-w-xl">file content</div>
+      {/* file permissions */}
+      <div className="flex flex-col gap-10 items-start min-w-xl lg:py-10">
+        {/* GRUPOS */}
+        <div className="flex flex-col w-full gap-6">
+          <h1 className="font-josefin text-xl text-sky-500">
+            Grupos com acesso
+          </h1>
+
+          <div className="flex flex-col gap-2">
+            {file.groups?.map((group) => (
+              <div className="flex justify-between text-zinc-500 py-2 px-4 border-2 border-zinc-500 rounded-lg">
+                <span className="font-gabarito ">{group.name}</span>
+                <X />
+              </div>
+            ))}
+            <div>Administrativo</div>
+          </div>
+        </div>
+        {/* USUÁRIOS */}
+        <div className="flex flex-col w-full gap-6">
+          <h1 className="font-josefin text-xl text-sky-500">
+            Usuários com acesso
+          </h1>
+          <div className="flex flex-col gap-2">
+            <div></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
