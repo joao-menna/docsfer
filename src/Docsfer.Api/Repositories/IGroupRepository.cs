910dcf31efc228ae027fb9cf5cdc1b1dad9e0d77
João Menna <joao.menna@docspider.com.br>
2025-11-19 12:02:18 -0300

    Made associate user to group endpoint

--- src/Docsfer.Api/Repositories/IGroupRepository.cs
+++ src/Docsfer.Api/Repositories/IGroupRepository.cs
@@ -1,8 +1,10 @@
 using Docsfer.Core.Groups;
+using Docsfer.Core.Identity;
 
 namespace Docsfer.Api.Repositories;
 
 public interface IGroupRepository
 {
     public Task<Group?> FindByIdAsync(Guid id);
+    public Task AssociateAsync(User user, Guid groupId);
 }
